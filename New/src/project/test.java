64bd883e3396f9ae8ffb7d73fe5bf6e6a8a436d4
PC-11 <PC-11@DESKTOP-E30QO8D>
2021-07-06 20:12:20 +0900

    하늘

--- New/src/project/test.java
+++ New/src/project/test.java
@@ -4,6 +4,8 @@ public class test {
 	public static void main(String[] args) {
 		System.out.println("하이 hi");
 		System.out.println("하이 hi");
+		System.out.println("하이 하이하잏아힝ㄴ헝나ㅣ항니ㅏ흔이ㅏㄹㄴㅇㄴㅇㅌㅋ");
+		System.out.println("하이 hi");
 	}
 
 }
